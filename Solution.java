107abf35e2fc31e35959a1838e78f5e2335f2bb3
EasyHappiness <xkf123456789@gmail.com>
2023-02-06 04:47:26 +0000

    11

--- Solution.java
+++ Solution.java
@@ -1,5 +1,10 @@
-public class Solution{
+public class Solution {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int[][] matrix= {{3,4},   
+             {2,3},      
+             
+             
+             
+             {4,5},        {1,3},        {2,2},        {3,6},        {1,2},        {3,2},        {2,4}        }
     }
 }
