6b75a44bd5c7a60abaa2ceba3050e77fd48bf580
EasyHappiness <xkf123456789@gmail.com>
2023-02-19 13:53:15 +0000

    test

---
+++ main.js
@@ -0,0 +1,41 @@
+function getLongestSubString(str1, str2) {
+    let dpValue = 0;
+    let maxLen = 0;
+    let resultRow = 0;
+    let resultColumn = 0;
+    let row = 0;
+    let column = str1.length - 1;
+    let count = str1.length + str2.length - 1;
+    while (count > 0) {
+        let startRow = row;
+        let startColumn = column;
+        while (column < str1.length && row < str2.length) {
+            if (str1[column] === str2[row]) {
+                dpValue += 1;
+            } else {
+                dpValue = 0;
+            }
+            if (dpValue > maxLen) {
+                maxLen = dpValue;
+                resultRow = row;
+                resultColumn = column;
+            }
+            column++;
+            row++;
+        }
+        if (column > 0) {
+            column = startColumn - 1;
+            row = 0;
+        } else {
+            column = 0;
+            row = startRow + 1;
+        }
+        count--;
+    }
+
+    return maxLen;
+}
+
+const str1 = '1AB2345CD';
+const str2 = '12345EF';
+console.log(getLongestSubString(str1, str2));
