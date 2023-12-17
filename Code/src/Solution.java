62acda44e4691e305883deabe4b12d03f5e85b96
EasyHappiness <xkf123456789@gmail.com>
2023-12-17 00:47:53 +0000

    code

--- Code/src/Solution.java
+++ Code/src/Solution.java
@@ -1,25 +1,56 @@
-public class Solution {
-    public int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
-        int width1 = ax2 - ax1;
-        int height1 = ay2 - ay1;
-        int area1 = width1 * height1;
-        int width2 = bx2 - bx1;
-        int height2 = by2 - by1;
-        int area2 = width2 * height2;
-        return area1 + area2 - getOverlapArea(ax1, ay1, ax2, ay2, bx1, by1, bx2, by2);
-    }
+import java.util.LinkedList;
 
-    private int getOverlapArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
-        int overlapWidth = getOverlapNum(ax1, ax2, bx1, bx2);
-        int overlapHeight = getOverlapNum(ay1, ay2, by1, by2);
-        if (overlapWidth > 0 && overlapHeight > 0) {
-            return overlapWidth * overlapHeight;
-        } else {
-            return 0;
+class Solution {
+    private LinkedList<String> symbolStack = new LinkedList<>();
+    private LinkedList<Integer> numStack = new LinkedList<>();
+    private int index = 0;
+
+    public int calculate(String s) {
+        while (index < s.length()) {
+            switch (s.charAt(index)) {
+                case '+':
+                    break;
+                case '-':
+                    break;
+                case '(':
+                    break;
+                case ')':
+                    break;
+                case ' ':
+                    index++;
+                    break;
+                default:
+                    break;
+            }
         }
+        return numStack.peek();
     }
 
-    private int getOverlapNum(int a1, int a2, int b1, int b2) {
-        return Math.min(a2, b2) - Math.max(a1, b1);
+    private int getNum(String s) {
+        int result = 0;
+        for (; index < s.length(); index++) {
+            char c = s.charAt(index);
+            if (c == ' ') {
+                continue;
+            }
+            if (Character.isDigit(c)) {
+                result = result * 10 + (c - '0');
+            }
+        }
+        return result;
+    }
+
+    private void calculateOnce() {
+        if (numStack.size() < 2 || symbolStack.isEmpty()) {
+            return;
+        }
+        int num1 = numStack.pop();
+        int num2 = numStack.pop();
+        String symbol = symbolStack.pop();
+        if (symbol.equals("+")) {
+            numStack.push(num1 + num2);
+        } else if (symbol.equals("-")) {
+            numStack.push(num1 - num2);
+        }
     }
 }
