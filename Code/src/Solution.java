acb7828405e64c2c51838dccdeebba33cc3602d8
EasyHappiness <xkf123456789@gmail.com>
2023-12-17 05:19:34 +0000

    code

--- Code/src/Solution.java
+++ Code/src/Solution.java
@@ -6,43 +6,19 @@ public class Solution {
     private int index = 0;
 
     public int calculate(String s) {
-        s = s.replace(" ", "");
         while (index < s.length()) {
-            switch (s.charAt(index)) {
-                case '+':
-                    while (!symbolStack.isEmpty() && symbolStack.peek() == '-') {
-                        calculateOnce();
-                    }
-                    symbolStack.push('+');
-                    index++;
-                    break;
-                case '-':
-                    if (index == 0 || s.charAt(index - 1) == '(') {
-                        numStack.push(0);
-                    }
-                    while (!symbolStack.isEmpty() && symbolStack.peek() != '(') {
-                        calculateOnce();
-                    }
-                    symbolStack.push('-');
-                    index++;
-                    break;
-                case '(':
-                    symbolStack.push('(');
-                    index++;
-                    break;
-                case ')':
-                    while (symbolStack.peek() != '(') {
-                        calculateOnce();
-                    }
-                    symbolStack.pop();
-                    index++;
-                    break;
-                case ' ':
-                    index++;
-                    break;
-                default:
-                    numStack.push(getNum(s));
-                    break;
+            char c = s.charAt(index);
+            if (Character.isDigit(c)) {
+                numStack.push(getNum(s));
+            } else if (c == ' ') {
+                index++;
+            } else {
+                int currentSymbolLevel = getSymbolLevel(c);
+                while (!symbolStack.isEmpty() && getSymbolLevel(symbolStack.peek()) >= currentSymbolLevel) {
+                    calculateOnce();
+                }
+                symbolStack.push(c);
+                index++;
             }
         }
         while (!symbolStack.isEmpty()) {
@@ -71,10 +47,36 @@ public class Solution {
         int numRight = numStack.pop();
         int numLeft = numStack.pop();
         char symbol = symbolStack.pop();
-        if (symbol == '+') {
-            numStack.push(numLeft + numRight);
-        } else if (symbol == '-') {
-            numStack.push(numLeft - numRight);
+        switch (symbol) {
+            case '+':
+                numStack.push(numLeft + numRight);
+                break;
+            case '-':
+                numStack.push(numLeft - numRight);
+                break;
+            case '*':
+                numStack.push(numLeft * numRight);
+                break;
+            case '/':
+                numStack.push(numLeft / numRight);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private int getSymbolLevel(char symbol) {
+        switch (symbol) {
+            case '+':
+                return 1;
+            case '-':
+                return 1;
+            case '*':
+                return 2;
+            case '/':
+                return 2;
+            default:
+                return 0;
         }
     }
 }
--- Code/src/Tester.java
+++ Code/src/Tester.java
@@ -12,24 +12,4 @@ public class Tester {
     public void tes2() {
         assert solution.calculate(" 2-1 + 2 ") == 3;
     }
-
-    @Test
-    public void tes3() {
-        assert solution.calculate("(1+(4+5+2)-3)+(6+8)") == 23;
-    }
-
-    @Test
-    public void test4() {
-        assert solution.calculate("1-(     -2)") == 3;
-    }
-
-    @Test
-    public void test5() {
-        assert solution.calculate("2-(5-6)") == 3;
-    }
-
-    @Test
-    public void test6() {
-        assert solution.calculate("(6)-(8)-(7)+(1+(6))") == -2;
-    }
 }
